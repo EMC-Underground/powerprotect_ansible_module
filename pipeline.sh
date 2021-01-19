b29fae2733249dd4806384328ee5435e1598ecb9
brad.soper@gmail.com <brad.soper@gmail.com>
2021-01-19 14:15:27 -0800

    updated pipeline with some comments and echo

--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
+echo "Logging in to PPDM"
 source ./add_k8s_cluster.sh \
 login \
+echo "Creating the Kubernetes Credentials"
 createkubecred \
 getkubecert \
 trustkubecert
