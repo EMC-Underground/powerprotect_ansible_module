b8615949acbe3be6279ffad41872227a980ccdbe
brad.soper@gmail.com <brad.soper@gmail.com>
2021-01-19 14:20:52 -0800

    removed back slashes from pipeline file

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Logging in to PPDM"
-source ./add_k8s_cluster.sh \
-login \
+source ./add_k8s_cluster.sh
+login
 echo "Creating the Kubernetes Credentials"
-createkubecred \
-getkubecert \
+createkubecred
+getkubecert
 trustkubecert
