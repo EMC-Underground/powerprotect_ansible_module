989b6064552be005e4480cd566c9d5929463bccf
brad.soper@gmail.com <brad.soper@gmail.com>
2021-01-19 08:07:51 -0800

    changing all curl calls to functions

--- add_k8s_cluster.sh
+++ add_k8s_cluster.sh
@@ -3,7 +3,10 @@
 password=Password#1
 server='10.237.198.35'
 
-kubepassword="eyJhbGciOiJSUzI1NiIsImtpZCI6IkF5M2dFYWxxY19rQmNQc2o5a2xGQktXWVB0dWtCRXl4cklwVXpCcVRiTFUifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJwb3dlcnByb3RlY3QiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoicHBkbS1kaXNjb3Zlcnktc2VydmljZWFjY291bnQtdG9rZW4tNHg5Z3giLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoicHBkbS1kaXNjb3Zlcnktc2VydmljZWFjY291bnQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiJjZGY1M2ZkZC1hMmJkLTQ2NzctODc4NS01NTY1NzM0NWMwYTciLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6cG93ZXJwcm90ZWN0OnBwZG0tZGlzY292ZXJ5LXNlcnZpY2VhY2NvdW50In0.BR9OE_izxq3Y1Qf9loaghJPj5BK55RqAUk5UHLqzZpO_JygQS1dcCKNMDpF9busBKOF4eQ_DKowbDxJLgHKl8Ikz6waCULL3dYyiHSu2okseXppF4kcOtvoM4_WruLhvltrDpU8DXaM5c_sjz7TPqtecvZAoS6pu_u0x2plahBxyV5uLpEBcEyZoemHyuPSxdBWDT68i9vZX-MzsfRroYKgPNmak4144YClr5OaXTNg55QtQEGQbWX-UoIZwLvDzJ8OaauuskhsOdo4Uf75WLYBic-0bfYbDuSAikrWkjq1bCMaCecEWwZH_8cM35eWuZ9fRFaZ8ukwjr2C5oo03Hw"
+kubepassword=$(kubectl get secret -n powerprotect $(kubectl get serviceaccount \
+-n powerprotect ppdm-discovery-serviceaccount -o jsonpath='{.secrets[0].name}') \
+-o jsonpath='{.data.token}')
+
 kubeusername=kubeuser
 credname=kubecreds02
 
@@ -14,14 +17,17 @@ kubeport=6443
 
 
 # LOGIN AND GET THE TOKEN
-request=$(curl -k --location --request POST https://${server}:8443/api/v2/login \
+login() { request=$(curl -k --location --request POST https://${server}:8443/api/v2/login \
 --header 'Content-Type: application/json' \
 --data "{"\"username\"":"\"admin\"","\"password\"":"\"$password\""}")
 
 token=$(echo $request | jq -r .access_token)
+echo $token
+}
 
 # GET ALL OF THE CREDS FROM PPDM
-echo " GETING ALL OF THE CREDS FROM PPDM"
+
+getallcreds() { echo " GETING ALL OF THE CREDS FROM PPDM"
 existingcreds=$( curl -k \
   --request GET \
   --url "https://${server}:8443/api/v2/credentials?pageSize=100&page=1" \
@@ -29,13 +35,10 @@ existingcreds=$( curl -k \
 
 echo "PRINTING OUT ALL THE CREDS FOUND"
 echo ${existingcreds}
+}
 
 # CREATE A CRED IN THIS CASE FOR KUBERNETES
-ppdmsatoken=$(kubectl get secret -n powerprotect $(kubectl get serviceaccount \
--n powerprotect ppdm-discovery-serviceaccount -o jsonpath='{.secrets[0].name}') \
--o jsonpath='{.data.token}')
-
-json_payload=$( jq -n \
+createkubecred() { json_payload=$( jq -n \
                   --arg kubepass "$kubepassword" \
                   --arg kubeuser "$kubeusername" \
                   --arg credname "$credname" \
@@ -52,46 +55,52 @@ curl -k \
   --header "Authorization: ${token}" \
   --header 'content-type: application/json' \
   --data "$json_payload"
+}
 
 # GET ALL KUBERNETES-CLUSTERS FROM PPDM
-existingkubeclusters=$(curl -k \
+
+getkubeclusters() {existingkubeclusters=$(curl -k \
   --request GET \
   --url "https://${server}:8443/api/v2/kubernetes-clusters" \
   --header "Authorization: ${token}")
 
 echo $existingkubeclusters
+}
 
 # RETRIEVE ALL INVENTORY SOURCES FROM PPDM
-existinginventory=$(curl -k \
+getallinventory() {existinginventory=$(curl -k \
   --request GET \
   --url "https://${server}:8443/api/v2/inventory-sources?pageSize=100&page=1" \
   --header "Authorization: ${token}")
 
 echo $existinginventory
-
+}
 # GET ALL CERTIFICATES
-kubecertpayload=$(curl -k \
+getallcerts() { kubecertpayload=$(curl -k \
   --request GET \
   --url "https://${server}:8443/api/v2/certificates?type=HOST" \
   --header "Authorization: ${token}")
-
+}
 # GET THE KUBE CERT
-kubecertpayload=$(curl -k \
+getkubecert() { kubecertpayload=$(curl -k \
   --request GET \
   --url "https://${server}:8443/api/v2/certificates?host=$kubeaddress&port=$kubeport&type=HOST" \
   --header "Authorization: ${token}")
+}
 
 
 # FUNCTION TO CLEAN UP THE CERT AND THE CREDS
 # DELETE THE KUBE CERT
-kubecertid=$(echo $kubecertpayload | jq -r '.id'
+deletekubecert() {
+  kubecertid=$(echo $kubecertpayload | jq -r '.id')
 curl -k \
   --request DELETE \
-  --url "https://${server}:8443/api/v2/certificats/$kubecertid" \
+  --url "https://${server}:8443/api/v2/certificats/${kubecertid}" \
   --header "Authorization: ${token}"
+}
 
 # TRUST THE KUBERNETES CERTIFICATE
-payload=$(echo $kubecertpayload | jq -r --arg a "$server" '.content[] | select(.host==$a) | .state = "ACCEPTED"')
+trustkubecert() { payload=$(echo $kubecertpayload | jq -r --arg a "$server" '.content[] | select(.host==$a) | .state = "ACCEPTED"')
 echo $payload
 
 curl -k \
@@ -100,15 +109,17 @@ curl -k \
   --header 'content-type: application/json' \
   --header "Authorization: ${token}" \
   --data "$payload"
+}
 
 # DELETE CREDENTIALS
-curl -k \
+deletecreds() { curl -k \
   --request DELETE \
   --url "https://${server}:8443/api/v2/${credid}" \
   --header "Authorization: ${token}"
+}
 
 # ADD AN INVENTORY SOURCE, SPECIFIALLY A KUBE CLUSTER
-getallids=$(echo $existingcreds | jq -r '.content[].id')
+addkubesource() { getallids=$(echo $existingcreds | jq -r '.content[].id')
 credid=$(echo $existingcreds | jq -r '.content[] | select(.type=="KUBERNETES") | .id')
 
 json_payload=$( jq -n \
@@ -127,5 +138,6 @@ curl -k \
   --header "Authorization: ${token}" \
   --header 'content-type: application/json' \
   --data "${json_payload}"
+}
 
 
