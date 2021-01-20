31a5266fd0d13ed50e7929ade94277ad393fec59
brad.soper@gmail.com <brad.soper@gmail.com>
2021-01-20 09:02:48 -0800

    update pipeline script and added a git ignore file

--- pipeline.sh
+++ pipeline.sh
@@ -5,5 +5,7 @@ source ./add_k8s_cluster.sh
 login
 echo "Creating the Kubernetes Credentials"
 createkubecred
+echo "Grabbing the Kubernetes Certificate"
 getkubecert
+echo "Trusting the Kubernetes Certificate"
 trustkubecert
